b6ccb66a87de8a18e82ad87d8ce343b89fee1c7a
mikeB <mike.f.bagheri@gmail.com>
2018-11-29 12:27:45 -0500

    fixed process.env syntax

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_RL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-      host: "localhost",
+      host: "host",
       port: 3306,
       user: "root",
       password: "1qazMKO)",
